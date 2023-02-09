222f64f24e3d1510960f4fce74d3e0bf0d9ec9c7
Matej Crnković <matej.crnkovic@avl.com>
2023-02-09 17:03:24 +0100

    dir structure changes

--- BrickBreaker/solution.py
+++ BrickBreaker/solution.py
@@ -1,7 +1,8 @@
 import pygame
+import random
 pygame.init()
 
-WIDTH, HEIGHT = 700, 500
+WIDTH, HEIGHT = 700, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT)) 
 pygame.display.set_caption("Brick Braker")
 
@@ -12,11 +13,11 @@ BLACK = (0,0,0)
 PADDLE_HEIGHT, PADDLE_WIDTH = 20, 100 
 BALL_RADIUS = 7
 
-S_FONT = pygame.font.SysFont('arial', 50)
+S_FONT = pygame.font.SysFont('comicsans', 50)
 
 class Paddle:
     COLOUR = WHITE
-    VEL = 5
+    VEL = 10
     def __init__(self, x ,y, width, height):
         self.x = x
         self.y = y
@@ -40,7 +41,8 @@ class Ball:
         self.x = self.original_x = x
         self.y = self.original_y = y
         self.radius = radius    
-        self.x_vel = 0
+        #self.x_vel = 4
+        self.x_vel = random.randint(-6, 6)
         self.y_vel = self.MAX_VEL
 
     def draw(self, win):
@@ -53,7 +55,8 @@ class Ball:
     def reset(self):
         self.x = self.original_x 
         self.y = self.original_y 
-        self.x_vel = 0
+        #self.x_vel = 4
+        self.x_vel = random.randint(-6, 6)
         self.y_vel = self.MAX_VEL
 
 class Brick:
@@ -69,28 +72,54 @@ class Brick:
         pygame.draw.rect(win, self.colour, (self.x , self.y, self.width, self.height))
 
     def collide(self, ball):
-        if(ball.y <= self.y + self.height and ball.y >= self.y and ball.x >= self.x and ball.x <= self.x + self.width):
+        #from above 
+        if 0 < self.y - ball.y <= ball.radius and self.x - ball.radius/2 <= ball.x <= self.x + self.width + ball.radius/2: 
             self.hit = True
             ball.y_vel *= -1
+            return True
+        #from below
+        elif 0 < ball.y - self.y - self.height <= ball.radius and self.x - ball.radius/2 <= ball.x <= self.x + self.width + ball.radius/2: 
+            self.hit = True
+            ball.y_vel *= -1
+            return True
+        #from left
+        elif 0 < self.x - ball.x <= ball.radius and self.y - ball.radius <= ball.y <= self.y + self.height + ball.radius:
+            self.hit = True
+            ball.x_vel *= -1
+            return True
+        #from right
+        elif 0 < ball.x - self.x - self.width <= ball.radius and self.y - ball.radius <= ball.y <= self.y + self.height + ball.radius:
+            self.hit = True
+            ball.x_vel *= -1
+            return True
+        """ elif(ball.y <= self.y + self.height and ball.y >= self.y and ball.x >= self.x and ball.x <= self.x + self.width):
+            self.hit = True
+            ball.y_vel *= -1
+            return True
         
         elif(ball.x + ball.radius > self.x and ball.y <= self.y + self.height and ball.y + ball.radius <= self.y):
             self.hit = True
             ball.x_vel *= -1
+            return True
 
         elif(ball.x <= self.x + self.width and ball.x >= self.x + ball.radius  and ball.y <= self.y + self.height and ball.y + ball.radius <= self.y):
             self.hit = True
             ball.x_vel *= -1
+            return True """
+        return False
 
 def draw(win, paddle, ball, hits, bricks):
 
-    hits_text = S_FONT.render(f"{hits}", 1, WHITE)
-    win.blit(hits_text, (WIDTH//2 , 15))
     win.fill(BLACK)
     paddle.draw(win)
     ball.draw(win)
 
     for brick in bricks:
             brick.draw(win)
+
+    hits_text = S_FONT.render(f"Score: {hits}", 1, (255, 0, 0))
+    win.blit(hits_text, (WIDTH//2 -  hits_text.get_width()/2, 15))
+
     pygame.display.update()
 
 def generate_bricks(rows, cols):
@@ -107,7 +136,7 @@ def generate_bricks(rows, cols):
 
 def handle_collision(ball, paddle):
     if ball.y + ball.radius >= HEIGHT:
-        ball.reset()
+        return False
     elif ball.y - ball.radius <= 0:
         ball.y_vel *= -1
     
@@ -127,6 +156,7 @@ def handle_collision(ball, paddle):
                 redu_fact = (paddle.width / 2) / ball.MAX_VEL
                 x_vel = diff_in_x / redu_fact
                 ball.x_vel = x_vel
+    return True
 
 def handle_paddle_movement(keys, paddle):
     if keys[pygame.K_RIGHT] and paddle.x + PADDLE_WIDTH+ paddle.VEL <= WIDTH:
@@ -144,7 +174,7 @@ def main():
 
     score = 0
     hits = 0
-    bricks = generate_bricks(7, 10)
+    bricks = generate_bricks(15, 10)
 
     while run:
         clock.tick(FPS)
@@ -158,13 +188,26 @@ def main():
         handle_paddle_movement(keys, paddle)   
 
         ball.move()
-        handle_collision(ball, paddle)
+        if not handle_collision(ball, paddle):
+            lose_text = S_FONT.render(f"You Lost! Your score is {hits}", 1, (255, 0, 0))
+            WIN.blit(lose_text, (WIDTH//2 -  lose_text.get_width()/2, HEIGHT//2 - lose_text.get_width()/2))
+            pygame.display.update()
+            pygame.time.delay(1000)
+            break
 
         for brick in bricks[:]:
-            brick.collide(ball)
-            if brick.hit == True:
-                bricks.remove(brick)
-
+            if(brick.collide(ball)):
+                hits +=1
+                if brick.hit == True:
+                    bricks.remove(brick)
+                break
+        
+        if hits == 150:
+            win_text = S_FONT.render(f"You have WON!", 1, (255, 0, 0))
+            WIN.blit(win_text, (WIDTH//2 -  win_text.get_width()/2, HEIGHT//2 - win_text.get_width()/2))
+            pygame.display.update()
+            pygame.time.delay(1000)
+            break
     pygame.quit()
     quit()
 
