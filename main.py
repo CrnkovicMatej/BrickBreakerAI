5e197b5d724e6ef4a6c489b250f098ab50966a20
Matej Crnković <matej.crnkovic@avl.com>
2023-03-03 15:35:05 +0100

    test best ai function added

--- main.py
+++ main.py
@@ -12,16 +12,25 @@ class BrickBreakerGame:
         self.ball = self.game.ball
         self.bricks = self.game.bricks
 
-    def test_ai(self):
+    def test_ai(self, net):
         run = True
         clock = pygame.time.Clock()
         while run:
+            game_info = self.game.loop()
             clock.tick(60)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
                     break
-            self.game.loop()
+            inputList = [self.paddle.x, self.paddle.y, self.game.ball.x, self.game.ball.y, self.game.ball.x_vel, self.game.ball.y_vel]
+            inputList.append(game_info.score)
+            inputList.append(game_info.hits)
+            output = net.activate(tuple(inputList))
+            if output[0] > 0.5 and self.paddle.x - self.paddle.VEL >= 0:
+                self.game.paddle.move(right=False)
+            if output[1] > 0.5 and  self.paddle.x + self.game.PADDLE_WIDTH + self.paddle.VEL <= self.game.WIDTH:
+                self.game.paddle.move(right=True)
+
             pygame.display.update()
 
         pygame.quit()
@@ -72,7 +81,7 @@ class BrickBreakerGame:
         genome1.fitness += game_info.score
         genome1.fitness += game_info.hits"""
 
-gen = 0
+gen = 129
 def eval_genomes(genomes, config):
     """for i, (genome_id, genome) in enumerate(genomes):
         genome.fitness = 0
@@ -163,7 +172,7 @@ def eval_genomes(genomes, config):
 
 
 def run_neat(config):
-    pop = neat.Checkpointer.restore_checkpoint('neat-checkpoint-78')
+    pop = neat.Checkpointer.restore_checkpoint('neat-checkpoint-129')
     #pop = neat.Population(config)
     pop.add_reporter(neat.StdOutReporter(True))
     stats = neat.StatisticsReporter()
@@ -173,12 +182,20 @@ def run_neat(config):
     with open('winner.pkl', 'wb') as f:
         pickle.dump(winner, f)
 
+def test_best_network(config):
+    with open("best.pickle", "rb") as f:
+        winner = pickle.load(f)
+    winner_net = neat.nn.FeedForwardNetwork.create(winner, config)
+
+    brick_breaker = BrickBreakerGame()
+    brick_breaker.test_ai(winner_net)
+
 if __name__ == "__main__":
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, "config.txt")
     config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
     run_neat(config)
-
+    #test_best_network(config)
 
 """import pygame
 import random
